bb3b906b133e2563ce588ad8320c776f37fe27de
Richard Bailey <richard@firsttech.digital>
2017-10-03 15:04:05 +0200

    added `IsNotNullOrEmpty` as well as `Split` extension methods #hacktoberfest #4

---
+++ ShittyLINQ/IsNotNullOrEmpty.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ShittyLINQ
+{
+    public partial class Extensions
+    {
+        /// <summary>
+        /// Determines whether the enumerable is not null or empty.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">The source.</param>
+        /// <returns>
+        ///   <c>false</c> if given collection is null or empty otherwise it returns <c>true</c>.
+        /// </returns>
+        public static bool IsNotNullOrEmpty<T>(this IEnumerable<T> source)
+        {
+            return source != null && source.Any();
+        }
+    }
+}
