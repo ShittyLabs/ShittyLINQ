f548f185cf94a819503bc2640053da392ea60229
Geir-Ove Bøe <goggenb@gmail.com>
2017-10-07 21:23:39 +0200

    Extension to check if an enumerable contains all items in another enumerable

---
+++ ShittyLINQ/Contains.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ShittyLINQ
+{
+    public static partial class Extensions
+    {
+        public static bool Contains<T>(this IEnumerable<T> self, IEnumerable<T> items)
+        {
+            return items.All(item => self.Contains(item));
+        }
+    }
+}
