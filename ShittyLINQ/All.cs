dd8fd708ee2288ad068cc2937f1fc8f6b24092cf
Patrick Tone <pt@patrickt.one>
2017-10-14 07:41:31 -0500

    Added `All` extension method and tests

---
+++ ShittyLINQ/All.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShittyLINQ
+{
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// True represents that each item satisfied the given condition.
+        /// </summary>
+        public static bool All<T>(this IEnumerable<T> self, Func<T, bool> predicate)
+        {
+            if (predicate == null)
+            {
+                throw new ArgumentNullException("The predicate is required.");
+            }
+
+            Func<bool, T, bool> function = (memo, obj) =>
+            {
+                memo &= predicate(obj);
+
+                return memo;
+            };
+
+            return self.Foldl(true, function);
+        }
+    }
+}
---
+++ ShittyLinqTests/AllTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShittyLINQ;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShittyTests
+{
+    [TestClass]
+    public class AllTests
+    {
+        [TestMethod]
+        public void All_OddNumbers()
+        {
+            IEnumerable<int> numbers = new[] { 1, 3, 5, 7, 9 };
+            bool expected = true;
+
+            bool actual = numbers.All(number => number % 2 == 1);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void All_OddNumbersWithOneEven()
+        {
+            IEnumerable<int> numbers = new[] { 1, 3, 4, 7, 9 };
+            bool expected = false;
+
+            bool actual = numbers.All(number => number % 2 == 1);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void All_ArgumentNullExceptionIfNoPredicate()
+        {
+            IEnumerable<int> numbers = Enumerable.Range(0, 5);
+
+            Assert.ThrowsException<ArgumentNullException>(() => numbers.All(null));
+        }
+    }
+}
