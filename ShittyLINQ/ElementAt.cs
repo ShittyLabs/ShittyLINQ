2176dffcce92545ef1251386d63b0c3691912f72
cooray <corycarlson209@gmail.com>
2017-10-15 11:49:43 -0500

    Added ElemetAt Extension

---
+++ ShittyLINQ/ElementAt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShittyLINQ
+{
+    public static partial class Extensions
+    {
+        public static T ElementAt<T>(this IEnumerable<T> self, int elementIndex)
+        {
+            var index = 0;
+            foreach (var item in self)
+            {
+                if (index == elementIndex)
+                {
+                    return item;
+                }
+                index++;
+            }
+            return default(T);
+        }
+    }
+}
