dcc1b9b1890814ab18375f3fe0bbc9af4472040f
Johnny Reina <johnny.r.reina@gmail.com>
2017-10-25 22:27:37 -0500

    Adds the Zip method. Tests forthcoming.

---
+++ ShittyLINQ/Zip.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShittyLINQ
+{
+    public static partial class Extensions
+    {
+        public static IEnumerable<Tuple<T, U>> Zip<T, U>(this IEnumerable<T> source, IEnumerable<U> second)
+        {
+            if (source == null || second == null) throw new ArgumentNullException();
+
+            var firstEnumerator = source.GetEnumerator();
+            var secondEnumerator = second.GetEnumerator();
+
+            while(firstEnumerator.MoveNext() && secondEnumerator.MoveNext())
+            {
+                yield return Tuple.Create(firstEnumerator.Current, secondEnumerator.Current);
+            }
+        }
+    }
+}
