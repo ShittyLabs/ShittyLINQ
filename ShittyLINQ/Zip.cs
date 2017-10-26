6cb7d859271d0840ea633286ae535ed56e1f3271
Johnny Reina <johnny.r.reina@gmail.com>
2017-10-25 23:09:14 -0500

    Adds XML documentation for .Zip()

--- ShittyLINQ/Zip.cs
+++ ShittyLINQ/Zip.cs
@@ -5,6 +5,16 @@ namespace ShittyLINQ
 {
     public static partial class Extensions
     {
+        /// <summary>
+        /// <para>
+        /// Combines two Enumerable objects into a sequence of Tuples containing each element
+        /// of the source Enumerable in the first position with the element that has the same
+        /// index in the second Enumerable in the second position.
+        /// </para>
+        /// <para>
+        /// The output sequence will be as long as the shortest input sequence.
+        /// </para>
+        /// </summary>
         public static IEnumerable<Tuple<T, U>> Zip<T, U>(this IEnumerable<T> source, IEnumerable<U> second)
         {
             if (source == null || second == null) throw new ArgumentNullException();
