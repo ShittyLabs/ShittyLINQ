221eb021e2a9be7020510c4c72f42665204dd248
Marc Max MALKA <1161961+TheNoobCompany@users.noreply.github.com>
2018-10-01 22:13:32 +0200

    Added ConcatTests and Concat extension

---
+++ ShittyLINQ/Concat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShittyLINQ
+{
+    public static partial class Extensions
+    {
+        public static IEnumerable<T> Concat<T>(this IEnumerable<T> source, IEnumerable<T> input)
+        {
+            if (source == null || input == null) throw new ArgumentNullException();
+            List<T> target = new List<T>(source);
+            target.AddRange(input);
+
+            return target;
+        }
+    }
+}
---
+++ ShittyLinqTests/ConcatTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShittyLINQ;
+using ShittyTests.TestHelpers;
+
+namespace ShittyTests
+{
+    [TestClass]
+    public class ConcatTests
+    {
+        [TestMethod]
+        public void Concat_SourceSequenceIsNull()
+        {
+            IEnumerable<int> source = null;
+            IEnumerable<int> input = new List<int>();
+            Assert.ThrowsException<ArgumentNullException>(() => source.Concat(input));
+        }
+
+        [TestMethod]
+        public void Concat_InputSequenceIsNull()
+        {
+            IEnumerable<int> source = new List<int>();
+            IEnumerable<int> input = null;
+            Assert.ThrowsException<ArgumentNullException>(() => source.Concat(input));
+        }
+
+        [TestMethod]
+        public void Concat_SequenceEquals()
+        {
+            var expected = new int[] { 0, 1, 2, 3, 4, 5 };
+            var source = new int[] { 0, 1, 2 };
+            var input = new int[] { 3, 4, 5 };
+            var actual = source.Concat(input);
+            TestHelper.AssertCollectionsAreSame(expected, actual);
+        }
+    }
+}
