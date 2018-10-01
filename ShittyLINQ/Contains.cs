a594af19ac1b6437c827925962d3543a8c6eb424
Santiago Rosenblatt <santiagorosenblatt@gmail.com>
2018-10-01 15:56:27 -0300

    Contains function with its corresponding tests was successfully added

---
+++ ShittyLINQ/Contains.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShittyLINQ
+{
+    public static partial class Extensions
+    {
+        public static bool Contains<T>(this IEnumerable<T> self, T element)
+        {
+            if (self == null || element == null) throw new ArgumentNullException();
+            foreach (var item in self)
+            {
+                if (item.Equals(element))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
