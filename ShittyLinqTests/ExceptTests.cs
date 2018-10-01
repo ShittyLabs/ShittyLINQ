c72f92c0f4f9158b4840346691bdb4990200f598
jw <icecreamburglar@gmail.com>
2018-10-01 13:44:22 -0500

    Except added

---
+++ ShittyLinqTests/ExceptTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShittyLINQ;
+using Enumerable = System.Linq.Enumerable;
+
+namespace ShittyTests
+{
+    [TestClass]
+    public class ExceptTests
+    {
+        [TestMethod]
+        public void Except_ReturnsExcept()
+        {
+            int[] first = { 1, 2, 3, 4, 5 };
+            int[] second = { 2, 4 };
+            int[] expectedResult = { 1, 3, 5 };
+
+            var result = first.Except(second);
+            Assert.IsTrue(Enumerable.SequenceEqual(expectedResult, result));
+        }
+        
+        [TestMethod]
+        public void Except_NoExceptions()
+        {
+            int[] first = { 1, 2, 3, 4, 5 };
+            int[] second = { 0, 6 };
+            int[] expectedResult = { 1, 2, 3, 4, 5 };
+
+            var result = first.Except(second);
+            Assert.IsTrue(Enumerable.SequenceEqual(expectedResult, result));
+        }
+
+        [TestMethod]
+        public void Except_AllExceptions()
+        {
+            int[] first = { 1, 2, 3, 4, 5 };
+            int[] second = { 1, 2, 3, 4, 5 };
+            int[] expectedResult = { };
+
+            var result = first.Except(second);
+            Assert.IsTrue(Enumerable.SequenceEqual(expectedResult, result));
+        }
+
+        [TestMethod]
+        public void Except_EmptyFirst()
+        {
+            int[] first = { };
+            int[] second = { 1, 2, 3 };
+            int[] expectedResult = { };
+
+            var result = first.Except(second);
+            Assert.IsTrue(Enumerable.SequenceEqual(expectedResult, result));
+        }
+
+        [TestMethod]
+        public void Except_EmptySecond()
+        {
+            int[] first = { 1, 2, 3, 4, 5 };
+            int[] second = { };
+            int[] expectedResult = { 1, 2, 3, 4, 5 };
+
+            var result = first.Except(second);
+            Assert.IsTrue(Enumerable.SequenceEqual(expectedResult, result));
+        }
+    }
+}
