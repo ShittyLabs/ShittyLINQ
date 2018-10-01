86c03aa21e05b92f5e8fc5d0b755b72b6ec36825
Christian Siber <siber.christian@gmail.com>
2018-10-01 22:32:05 +0200

    implement contains

---
+++ ShittyLINQ/Contains.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShittyLINQ
+{
+    public static partial class Extensions
+    {
+        public static bool Contains<T>(this IEnumerable<T> self, T element)
+        {
+            var enumerator = self.GetEnumerator();
+            while(enumerator.MoveNext())
+            {
+                if (EqualityComparer<T>.Default.Equals(enumerator.Current, element)) return true;
+            }
+            return false;
+        }
+
+        public static bool Contains<T>(this IEnumerable<T> self, T element, IEqualityComparer<T> equalityComparer)
+        {
+            var enumerator = self.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                if (equalityComparer.Equals(enumerator.Current, element)) return true;
+            }
+            return false;
+        }
+
+    }
+}
