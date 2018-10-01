5677da0b4253323f6af2309e869f570274bc2fe5
jw <icecreamburglar@gmail.com>
2018-10-01 14:08:20 -0500

    DefaultEmpty added

---
+++ ShittyLINQ/DefaultIfEmpty.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShittyLINQ
+{
+    public static partial class Extensions
+    {
+        public static IEnumerable<T> DefaultIfEmpty<T>(this IEnumerable<T> source)
+        {
+            return DefaultIfEmpty(source, default(T));
+        }
+
+        public static IEnumerable<T> DefaultIfEmpty<T>(this IEnumerable<T> self, T defaultValue)
+        {
+            if (defaultValue == null) throw new ArgumentNullException();
+            if (self == null || self.Count() <= 0)
+            {
+                yield return defaultValue;
+            }
+            else
+            {
+                var iterator = self.GetEnumerator();
+                while (iterator.MoveNext())
+                {
+                    yield return iterator.Current;
+                }
+            }
+        }
+    }
+}
---
+++ ShittyLinqTests/DefaultIfEmptyTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShittyLINQ;
+using Enumerable = System.Linq.Enumerable;
+
+namespace ShittyTests
+{
+    [TestClass]
+    public class DefaultIfEmptyTests
+    {
+        [TestMethod]
+        public void Default_NotEmpty()
+        {
+            int[] source = { 1, 2, 3 };
+            int[] expectedResult = { 1, 2, 3 };
+
+            var result = source.DefaultIfEmpty(0);
+            Assert.IsTrue(Enumerable.SequenceEqual(expectedResult, result));
+        }
+
+        [TestMethod]
+        public void Default_Empty()
+        {
+            int[] source = { };
+            int[] expectedResult = { 0 };
+
+            var result = source.DefaultIfEmpty(0);
+            Assert.IsTrue(Enumerable.SequenceEqual(expectedResult, result));
+        }
+
+        [TestMethod]
+        public void Default_EnumerableNull()
+        {
+            int[] source = null;
+            int[] expectedResult = { 0 };
+
+            var result = source.DefaultIfEmpty(0);
+            Assert.IsTrue(Enumerable.SequenceEqual(expectedResult, result));
+        }
+    }
+}
