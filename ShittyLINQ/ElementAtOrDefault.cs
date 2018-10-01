5ef4e857c13876609bbe612f0c452f08abadc3aa
Jean-Dominique Nguele <jean-dominique.nguele@bjss.com>
2018-10-01 19:12:06 +0100

    Add ElementAtOrDefault method and tests

---
+++ ShittyLINQ/ElementAtOrDefault.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShittyLINQ
+{
+    public static partial class Extensions
+    {
+        public static T ElementAtOrDefault<T>(this IEnumerable<T> self, int elementIndex)
+        {
+            try {
+                return ElementAt(self, elementIndex);
+            }
+            catch (ArgumentOutOfRangeException) {
+                return default(T);
+            }
+        }
+    }
+}
