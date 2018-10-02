c1eaf83f10cf542b981f529493d74d9f6128a3d4
Muhammad Azeez <muhammad-azeez@outlook.com>
2018-10-03 00:49:26 +0300

    Impelement SingleOrDefault

---
+++ ShittyLINQ/SingleOrDefault.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShittyLINQ
+{
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// Returns the first element of the sequence 
+        /// or the default value of the type if the collection is empty.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the items in the sequence</typeparam>
+        /// <param name="source">The source sequence</param>
+        /// <returns></returns>
+        public static TSource SingleOrDefault<TSource>(this IEnumerable<TSource> source)
+        {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+
+            var enumerator = source.GetEnumerator();
+            if (enumerator.MoveNext())
+            {
+                return enumerator.Current;
+            }
+
+            return default(TSource);
+        }
+    }
+}
---
+++ ShittyLinqTests/SingleOrDefaultTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace ShittyTests
+{
+    [TestClass]
+    public class SingleOrDefaultTests
+    {
+        [TestMethod]
+        public void SingleOrDefault_ReturnsTheFirstElementIfCollectionIsNotEmpty()
+        {
+            var expected = 42;
+            var collection = new int[] { 42, 1, 99 };
+
+            var first = collection.FirstOrDefault();
+            Assert.AreEqual(expected, first);
+        }
+
+        [TestMethod]
+        public void SingleOrDefault_ReturnsDefaultValueIfCollectionIsEmpty()
+        {
+            var collection = new int[] { };
+
+            var first = collection.FirstOrDefault();
+            Assert.AreEqual(default(int), first);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SingleOrDefault_ThrowsIfSourceIsNull()
+        {
+            IOrderedEnumerable<int> collection = null;
+            var first = collection.FirstOrDefault();
+        }
+    }
+}
