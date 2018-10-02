a5e82f9bcf4e594ad50c1a7c7f79c9ea334b84d4
Muhammad Azeez <muhammad-azeez@outlook.com>
2018-10-03 01:16:06 +0300

    Implement OfType

---
+++ ShittyLinqTests/OfTypeTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShittyLINQ;
+using ShittyTests.TestHelpers;
+using System;
+using System.Collections;
+
+namespace ShittyTests
+{
+    [TestClass]
+    public class OfTypeTests
+    {
+        [TestMethod]
+        public void OfType_FiltersItemsBasedOnType()
+        {
+            var items = new object[] { 1, "hello", 8, 24.12d, 89.15f, 62.8m, new object() };
+            var expected = new int[] { 1, 8 };
+            var ints = items.OfType<int>();
+
+            TestHelper.AssertCollectionsAreSame(expected, ints);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void OfType_ThrowsIfSourceSequenceIsEmpty()
+        {
+            IEnumerable items = null;
+            var ints = items.OfType<int>()?.ToList();
+        }
+    }
+}
