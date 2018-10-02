3791e5aab8f0a4361c604be937a2a0faf85109b4
Unknown <dcparga@gmail.com>
2018-10-02 20:32:59 +0200

    Added Union method

---
+++ ShittyLINQ/Union.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShittyLINQ
+{
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// Produces the set union of two sequences.
+        /// </summary>
+        /// <param name="first">First set of elements</param>
+        /// <param name="second">Second set of elements</param>
+        /// <param name="comparer">If specified, the comparer to compare objects</param>
+        /// <typeparam name="T">Type of the objects</typeparam>
+        /// <returns>A set containing all the different elements in both sets</returns>
+        /// <exception cref="ArgumentNullException">If first or second sets is null</exception>
+        public static IEnumerable<T> Union<T>(this IEnumerable<T> first, IEnumerable<T> second,
+            IEqualityComparer<T> comparer = null)
+        {
+            if (first == null || second == null) throw new ArgumentNullException();
+            
+            return UnionImpl(first, second, comparer ?? EqualityComparer<T>.Default);
+
+        }
+
+        private static IEnumerable<T> UnionImpl<T>(IEnumerable<T> first, IEnumerable<T> second,
+            IEqualityComparer<T> comparer)
+        {
+            var seenElements = new HashSet<T>(comparer); 
+            foreach (var item in first) 
+            { 
+                if (seenElements.Add(item)) 
+                { 
+                    yield return item; 
+                } 
+            } 
+            foreach (var item in second) 
+            { 
+                if (seenElements.Add(item)) 
+                { 
+                    yield return item; 
+                } 
+            } 
+        }
+    }
+}
---
+++ ShittyLinqTests/TestHelpers/PersonComparer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace ShittyTests.TestHelpers
+{
+    public class PersonComparer : IEqualityComparer<Person>
+    {
+        public bool Equals(Person x, Person y)
+        {
+            if (x == null && y == null)
+                return true;
+            if (x == null || y == null)
+                return false;
+            
+            return x.Age == y.Age && x.FavoriteColor == y.FavoriteColor && x.Job == y.Job && x.Name == y.Name;
+        }
+
+        public int GetHashCode(Person obj)
+        {
+            return $"{obj.Age}{obj.FavoriteColor}{obj.Job}{obj.Name}".GetHashCode();
+        }
+    }
+}
