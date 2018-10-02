877a992d58df9a4d1ea3c136fbf26d91ad94c6a9
Stephan Nielsen <stephannielsen@gmail.com>
2018-10-02 14:42:30 +0200

    Added SkipWhile method and tests.

---
+++ ShittyLINQ/SkipWhile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShittyLINQ
+{
+  public static partial class Extensions
+  {
+    public static IEnumerable<T> SkipWhile<T>(this IEnumerable<T> self, Func<T, bool> predicate)
+    {
+      if (self == null || predicate == null) throw new ArgumentNullException();
+
+      using (var enumerator = self.GetEnumerator())
+      {
+        while (enumerator.MoveNext())
+        {
+          if (!predicate(enumerator.Current))
+          {
+            yield return enumerator.Current;
+          }
+        }
+      }
+
+    }
+
+    public static IEnumerable<T> SkipWhile<T>(this IEnumerable<T> self, Func<T, int, bool> predicate)
+    {
+      if (self == null || predicate == null) throw new ArgumentNullException();
+
+      using (var enumerator = self.GetEnumerator())
+      {
+        var index = 0;
+        while (enumerator.MoveNext())
+        {
+          if (!predicate(enumerator.Current, index))
+          {
+            yield return enumerator.Current;
+          }
+          index++;
+        }
+      }
+
+    }
+  }
+}
