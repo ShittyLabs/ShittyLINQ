49879a3abc0a9e35d367a6a1d081ff72d9a7dff6
Muhammad Azeez <muhammad-azeez@outlook.com>
2018-10-02 18:00:56 +0300

    Implement TakeWhile

---
+++ ShittyLINQ/TakeWhile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShittyLINQ
+{
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// Returns elements from a sequence as long as the specified condition is true.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the source sequence.</typeparam>
+        /// <param name="source">The sequence to return th elements from.</param>
+        /// <param name="predicate">The predicates used to stop the function.</param>
+        /// <returns></returns>
+        public static IEnumerable<TSource> TakeWhile<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            foreach (var element in source)
+            {
+                if (!predicate(element))
+                    break;
+
+                yield return element;
+            }
+        }
+
+        /// <summary>
+        /// Returns elements from a sequence as long as the specified condition is true. 
+        /// The index of the current item is passed to the predicate function as well.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the source sequence.</typeparam>
+        /// <param name="source">The sequence to return th elements from.</param>
+        /// <param name="predicate">The predicates used to stop the function.</param>
+        /// <returns></returns>
+        public static IEnumerable<TSource> TakeWhile<TSource>(this IEnumerable<TSource> source, Func<TSource, int, bool> predicate)
+        {
+            int i = 0;
+            foreach (var element in source)
+            {
+                if (!predicate(element, i++))
+                    break;
+
+                yield return element;
+            }
+        }
+    }
+}
---
+++ ShittyLinqTests/TakeWhileTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShittyLINQ;
+using ShittyTests.TestHelpers;
+
+namespace ShittyTests
+{
+    [TestClass]
+    public class TakeWhileTests
+    {
+        [TestMethod]
+        public void TakeWhile_ReturnsASubsetIfConditionBecomesFalse()
+        {
+            var expected = new int[] { 1, 5, 6, 9 };
+            var items = new int[] { 1, 5, 6, 9, -2, 1, 5 };
+
+            var actual = items.TakeWhile(e => e > 0);
+            var actual2 = items.TakeWhile((e, i) => e > 0);
+            TestHelper.AssertCollectionsAreSame(expected, actual);
+            TestHelper.AssertCollectionsAreSame(expected, actual2);
+        }
+
+        [TestMethod]
+        public void TakeWhile_ReturnsAllElementsIfConditionIsMetUntilTheEnd()
+        {
+            var expected = new int[] { 1, 1, 99, 534, 123 };
+            var items = new int[] { 1, 1, 99, 534, 123 };
+
+            var actual = items.TakeWhile(e => e > 0);
+            var actual2 = items.TakeWhile((e, i) => e > 0);
+
+            TestHelper.AssertCollectionsAreSame(expected, actual);
+            TestHelper.AssertCollectionsAreSame(expected, actual2);
+        }
+
+        [TestMethod]
+        public void TakeWhile_PassedIndexIsCorrect()
+        {
+            var expected = new int[] { 0, 1, 2, 3, 4, 5 };
+            var items = new int[] { 0, 1, 2, 3, 4, 5 };
+
+            var actual = items.TakeWhile((e, i) => i == e);
+            TestHelper.AssertCollectionsAreSame(expected, actual);
+        }
+
+        [TestMethod]
+        public void TakeWhile_ReturnsNoneIfConditionIsNeverMet()
+        {
+            var expected = new int[] { };
+            var items = new int[] { 1, 1, 99, 534, 123 };
+
+            var actual = items.TakeWhile(e => e < 0);
+            var actual2 = items.TakeWhile((e, i) => e < 0);
+
+            Assert.AreEqual(actual.Count(), 0);
+            Assert.AreEqual(actual2.Count(), 0);
+        }
+    }
+}
