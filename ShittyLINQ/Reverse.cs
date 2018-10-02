5a54723d4d6520a1e379cb35b3d2f82deea95d89
Zarkopafilis <zarkopafilis@gmail.com>
2018-10-02 11:57:19 +0300

    Add Reverse and Tests

---
+++ ShittyLINQ/Reverse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShittyLINQ
+{
+    public static partial class Extensions
+    {
+        public static System.Collections.Generic.IEnumerable<TSource> Reverse<TSource>(this System.Collections.Generic.IEnumerable<TSource> source)
+        {
+            if (source == null) throw new ArgumentNullException();
+            
+            for(var i = (int) source.Count() - 1; i >= 0; i--)
+            {
+                yield return source.ElementAt(i);
+            }
+        }
+    }
+}
---
+++ ShittyLinqTests/ReverseTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using ShittyLINQ;
+using ShittyTests.TestHelpers;
+using System.Diagnostics;
+
+namespace ShittyTests
+{
+    [TestClass]
+    public class ReverseTests
+    {
+        [TestMethod]
+        public void Reverse_ReturnsCorrectElements()
+        {
+            IEnumerable<int> numbers = new List<int> { 5, 6, 7 };
+            var result = numbers.Reverse();
+            var expected = new List<int> { 7, 6, 5 };
+
+            TestHelper.AssertCollectionsAreSame(expected, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Reverse_SourceIsNull()
+        {
+            IEnumerable<int> numbers = null;
+            numbers.Reverse().ToList();
+        }
+
+        [TestMethod]
+        public void Reverse_SourceIsEmpty()
+        {
+            IEnumerable<int> numbers = new List<int> { };
+            var result = numbers.Reverse().ToList();
+
+            Assert.AreEqual(0, numbers.Count());
+        }
+    }
+}
