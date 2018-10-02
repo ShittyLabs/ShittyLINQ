11223056b719dcd367986d4a35de9aabf2617fd3
Unknown <dcparga@gmail.com>
2018-10-02 22:06:28 +0200

    ToDictionary functionality

---
+++ ShittyLINQ/ToDictionary.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShittyLINQ
+{
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// Creates a Dictionary from an IEnumerable 
+        /// </summary>
+        /// <param name="source">Set of elements to be converted to dictionary</param>
+        /// <param name="keySelector">Key selector to determine the key of the dictionary</param>
+        /// <typeparam name="TSource">Source type</typeparam>
+        /// <typeparam name="TKey">Key type</typeparam>
+        /// <returns>A dictionary made from source</returns>
+        /// <exception cref="ArgumentNullException">If source, keySelector or element Selector is null</exception>
+        public static Dictionary<TKey, TSource> ToDictionary<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector)
+        {
+            return ToDictionary(source, keySelector, t => t, EqualityComparer<TKey>.Default);
+        }
+
+        /// <summary>
+        /// Creates a Dictionary from an IEnumerable 
+        /// </summary>
+        /// <param name="source">Set of elements to be converted to dictionary</param>
+        /// <param name="keySelector">Key selector to determine the key of the dictionary</param>
+        /// <param name="elementSelector">Element selector to determine the value of the dictionary</param>
+        /// <typeparam name="TSource">Source type</typeparam>
+        /// <typeparam name="TKey">Key type</typeparam>
+        /// <typeparam name="TElement">Value type</typeparam>
+        /// <returns>A dictionary made from source</returns>
+        /// <exception cref="ArgumentNullException">If source, keySelector or element Selector is null</exception>
+        public static Dictionary<TKey, TElement> ToDictionary<TSource, TKey, TElement>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector,
+            Func<TSource, TElement> elementSelector)
+        {
+            return source.ToDictionary(keySelector, elementSelector, EqualityComparer<TKey>.Default);
+        }
+
+        /// <summary>
+        /// Creates a Dictionary from an IEnumerable 
+        /// </summary>
+        /// <param name="source">Set of elements to be converted to dictionary</param>
+        /// <param name="keySelector">Key selector to determine the key of the dictionary</param>
+        /// <param name="comparer">Comparer to be used to determine equality between elements</param>
+        /// <typeparam name="TSource">Source type</typeparam>
+        /// <typeparam name="TKey">Key type</typeparam>
+        /// <returns>A dictionary made from source</returns>
+        /// <exception cref="ArgumentNullException">If source, keySelector or element Selector is null</exception>
+        public static Dictionary<TKey, TSource> ToDictionary<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector,
+            IEqualityComparer<TKey> comparer)
+        {
+            return ToDictionary(source, keySelector, t => t, comparer);
+        }
+
+        /// <summary>
+        /// Creates a Dictionary from an IEnumerable 
+        /// </summary>
+        /// <param name="source">Set of elements to be converted to dictionary</param>
+        /// <param name="keySelector">Key selector to determine the key of the dictionary</param>
+        /// <param name="elementSelector">Element selector to determine the value of the dictionary</param>
+        /// <param name="comparer">Comparer to be used to determine equality between elements</param>
+        /// <typeparam name="TSource">Source type</typeparam>
+        /// <typeparam name="TKey">Key type</typeparam>
+        /// <typeparam name="TElement">Value type</typeparam>
+        /// <returns>A dictionary made from source</returns>
+        /// <exception cref="ArgumentNullException">If source, keySelector or element Selector is null</exception>
+        public static Dictionary<TKey, TElement> ToDictionary<TSource, TKey, TElement>( 
+            this IEnumerable<TSource> source, 
+            Func<TSource, TKey> keySelector, 
+            Func<TSource, TElement> elementSelector, 
+            IEqualityComparer<TKey> comparer) 
+        { 
+            if (source == null || keySelector == null || elementSelector == null) throw new ArgumentNullException();
+            
+            Dictionary<TKey, TElement> ret = new Dictionary<TKey, TElement>(comparer ?? EqualityComparer<TKey>.Default); 
+            foreach (TSource item in source) 
+            { 
+                ret.Add(keySelector(item), elementSelector(item)); 
+            } 
+            return ret; 
+        }   
+    }
+    
+}
---
+++ ShittyLinqTests/ToDictionaryTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShittyLINQ;
+using ShittyTests.TestHelpers;
+
+namespace ShittyTests
+{
+    [TestClass]
+    public class ToDictionaryTests
+    {
+        [TestMethod]
+        public void ToDictionary_Numbers()
+        {
+            IEnumerable<int> numbers = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+            var result = numbers.ToDictionary(x => x, x=> true);
+
+            Assert.AreEqual(result.Count, 9);
+            
+            foreach (int number in numbers)
+            {
+                Assert.IsTrue(result.ContainsKey(number));
+            }
+        }
+
+        [TestMethod]
+        public void ToDictionary_Person()
+        {
+            var adam = new Person("Adam", 20, "Arquitech", "Amber");
+            var brian = new Person("Brian", 45, "Arquitech", "Blue");
+            var charles = new Person("Charles", 32, "Arquitech", "Cyan");
+            var dani = new Person("Dani", 33, "Developer", "Deep Purple");
+            
+            IEnumerable<Person> people = new[] { adam, brian, charles, dani };
+
+            var result = people.ToDictionary(person => person.Age);
+
+            Assert.AreEqual(result.TryGetValue(brian.Age, out var expectedBrian), true);
+            Assert.IsNotNull(expectedBrian);
+            Assert.AreEqual(expectedBrian.Name, brian.Name);
+        }
+    }
+}
