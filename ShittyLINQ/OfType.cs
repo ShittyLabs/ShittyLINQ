de031b7f54e90fae7e0579b29ef5c9a8673c8d9d
Roger Leblanc <roger@rogerleblanc.net>
2018-10-03 00:51:31 -0400

    Add OfType method and tests

---
+++ ShittyLINQ/OfType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ShittyLINQ
+{
+    public static partial class Extensions
+    {
+        public static IEnumerable<TResult> OfType<TResult>(this IEnumerable self)
+        {
+            if (self == null) throw new ArgumentNullException();
+            return OfTypeIterator<TResult>(self);
+        }
+
+        private static IEnumerable<TResult> OfTypeIterator<TResult>(IEnumerable source)
+        {
+            foreach (object obj in source)
+            {
+                if (obj is TResult) yield return (TResult)obj;
+            }
+        }
+    }
+}
