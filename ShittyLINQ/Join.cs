00dd318208e82741a46e1540e7039b16b3e5e437
Johnny Reina <johnny.r.reina@gmail.com>
2018-10-14 00:17:17 -0500

    Added super flaky join method. Probably should've just let someone else do this one.

---
+++ ShittyLINQ/Join.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShittyLINQ
+{
+    public static partial class Extensions
+    {
+        public static IEnumerable<TResult> Join<TOuter, TInner, TKey, TResult>(this IEnumerable<TOuter> outer,
+            IEnumerable<TInner> inner,
+            Func<TOuter, TKey> outerKeySelector,
+            Func<TInner, TKey> innerKeySelector,
+            Func<TOuter, TInner, TResult> resultSelector)
+        {
+            if (outer == null || outerKeySelector == null || innerKeySelector == null || resultSelector == null) throw new ArgumentNullException();
+            var outerIterator = outer.GetEnumerator();
+
+            var comparer = EqualityComparer<TKey>.Default;
+            TOuter currentOuter = outerIterator.Current;
+
+            while (outerIterator.MoveNext())
+            {
+                currentOuter = outerIterator.Current;
+                TInner currentInner;
+                var innerIterator = inner.GetEnumerator();
+
+                // Find match from inner
+                while (innerIterator.MoveNext())
+                {
+                    currentInner = innerIterator.Current;
+                    if (comparer.Equals(outerKeySelector(currentOuter), innerKeySelector(currentInner)))
+                    {
+                        yield return resultSelector(currentOuter, currentInner);
+                    }
+                }
+            }
+        }
+    }
+}
