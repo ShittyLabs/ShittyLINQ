52e57e3c2958845a78d87b949de49901ad96e327
parsaManouchehrian <work.parsa.manouchehrian@gmail.com>
2018-10-20 14:47:19 -0700

    create anouther count

--- ShittyLINQ/Count.cs
+++ ShittyLINQ/Count.cs
@@ -10,5 +10,17 @@ namespace ShittyLINQ
         {
             return self.Aggregate(0L, (memo, v) => ++memo);
         }
+
+        public static long Count<T>(this IEnumerable<T> self , Func<T, bool> predicate)
+        {
+            if (self == null || predicate == null) throw new ArgumentNullException();
+            Func<List<T>, T, List<T>> mapFunc = (memo, obj) =>
+            {
+                if (predicate(obj)) memo.Add(obj);
+                return memo;
+            };
+            var output = self.Foldl(new List<T>(), mapFunc);
+            return output.Aggregate(0L, (memo, v) => ++memo);
+        }
     }
 }
