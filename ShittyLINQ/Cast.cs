783f8bba4d1d997c4625d22fdd6219578c324d89
poune05 <poune05@hotmail.com>
2018-10-20 21:59:27 +0200

    Add Cast extension method and tests #28

---
+++ ShittyLINQ/Cast.cs
@@ -0,0 +1,24 @@
+﻿namespace ShittyLINQ
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public static partial class Extensions
+    {
+        public static IEnumerable<TResult> Cast<TResult>(this IEnumerable source)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            
+            if (source is IEnumerable<TResult> results)
+            {
+                foreach (var result in results)
+                    yield return result;
+            }
+
+            foreach (TResult result in source)
+                yield return result;
+        }
+    }
+}
---
+++ ShittyLinqTests/CastTests.cs
@@ -0,0 +1,52 @@
+﻿namespace ShittyTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ShittyLINQ;
+    using ShittyTests.TestHelpers;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    [TestClass]
+    public class CastTests
+    {
+        [TestMethod]
+        public void Cast_ReturnsExpected()
+        {
+            var source = new ArrayList { "1", "2", "3" };
+            var expectedResult = new List<string> { "1", "2", "3" };
+
+
+            var result = source.Cast<string>();
+
+            TestHelper.AssertCollectionsAreSame(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Cast_ReturnsExpectedWithEmptyCollection()
+        {
+            var source = new ArrayList();
+            var expectedResult = new List<string>();
+
+            var result = source.Cast<string>();
+
+            TestHelper.AssertCollectionsAreSame(expectedResult, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Cast_CollectionIsNull()
+        {
+            ((ArrayList)null).Cast<string>().ToList();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidCastException))]
+        public void Cast_Invalid()
+        {
+            var source = new ArrayList { "1", "2", "3" };
+
+            source.Cast<char>().ToList();
+        }
+    }
+}
