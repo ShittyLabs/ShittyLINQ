8e6f565702e00707e03f948c52af1398d561985c
parsaManouchehrian <work.parsa.manouchehrian@gmail.com>
2018-10-20 14:34:49 -0700

    add FirstOrDefault

---
+++ ShittyLINQ/FirstOrDefault.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShittyLINQ
+{
+    public static partial class Extensions
+    {
+        public static T FirstOrDefault<T>(this IEnumerable<T> self)
+        {
+            if (self == null) throw new ArgumentNullException();
+            var iterator = self.GetEnumerator();
+            if (!iterator.MoveNext()) return default(T);
+            return iterator.Current;
+        }
+    }
+}
