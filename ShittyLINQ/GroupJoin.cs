a4c72fc27fbab94f78ce2684327864e4d4cfdc67
James Stoker <jstoker.github@jcs.me.uk>
2018-10-29 20:55:43 +0000

    Basic implementation of GroupJoin. Related to #46.

---
+++ ShittyLINQ/GroupJoin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShittyLINQ
+{
+    public static partial class Extensions
+    {
+        public static IEnumerable<TResult> GroupJoin<TOuter, TInner, TKey, TResult>(
+            this IEnumerable<TOuter> outer,
+            IEnumerable<TInner> inner,
+            Func<TOuter, TKey> outerKeySelector,
+            Func<TInner, TKey> innerKeySelector,
+            Func<TOuter, IEnumerable<TInner>, TResult> resultSelector,
+            IEqualityComparer<TKey> comparer)
+        { 
+            foreach (var outerItem in outer)
+            {
+                var outerKey = outerKeySelector(outerItem);
+                var innerItems = inner.Where(innerItem => comparer.Equals(outerKey, innerKeySelector(innerItem)));
+                yield return resultSelector(outerItem, innerItems);
+            }
+        }
+
+        public static IEnumerable<TResult> GroupJoin<TOuter, TInner, TKey, TResult>(
+            this IEnumerable<TOuter> outer,
+            IEnumerable<TInner> inner,
+            Func<TOuter, TKey> outerKeySelector,
+            Func<TInner, TKey> innerKeySelector,
+            Func<TOuter, IEnumerable<TInner>, TResult> resultSelector)
+        {
+           return outer.GroupJoin(inner, outerKeySelector, innerKeySelector, resultSelector, EqualityComparer<TKey>.Default);
+        }
+    }
+}
