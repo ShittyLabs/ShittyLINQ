00a69200c3c251dc6bc9be917068b1af4ae8573c
hatalaef <hatalaef@gmail.com>
2018-10-31 14:15:33 -0400

    Add Reverse method and Tests.

---
+++ ShittyLINQ/Reverse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShittyLINQ
+{
+    public static partial class Extensions
+    {
+        public static IEnumerable<T> Reverse<T>(this IEnumerable<T> self)
+        {
+            if (self == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            int last = (int)self.Count() - 1;
+            for(int i = last; i >=0; i--)
+            {
+                yield return self.ElementAt(i);
+            }
+        }
+    }
+}
---
+++ ShittyLinqTests/ReverseTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShittyLINQ;
+using System;
+using System.Collections.Generic;
+
+namespace ShittyTests
+{
+    [TestClass]
+    public class ReverseTests
+    {
+        [TestMethod]
+        public void Reverse_ReturnsReversed()
+        {
+            List<int> numberList = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var result = numberList.Reverse<int>().ToList();
+            var expected = new List<int> { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Skip_SourceIsNull()
+        {
+            List<int> numberList = null;
+
+            numberList.Reverse<int>().ToList();
+        }
+    }
+}
