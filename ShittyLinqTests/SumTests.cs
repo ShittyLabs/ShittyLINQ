e03d572e14bf96836cc75181dea412df89f3ad15
nestedl00p <nrosales@clubessential.com>
2018-10-31 23:57:01 -0500

    Sum added

---
+++ ShittyLinqTests/SumTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShittyLINQ;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShittyTests
+{
+    [TestClass]
+    public class SumTests
+    {
+        [TestMethod]
+        public void Sum_ThrowsExceptionWhenSourceIsNull()
+        {
+            List<int> numberList = null;
+            Assert.ThrowsException<ArgumentNullException>(() => numberList.Sum());
+        }
+
+        [TestMethod]
+        public void Sum_EmptyListReturnsZero()
+        {
+            List<int> numberList = new List<int>();
+            var result = numberList.Sum();
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Sum_SumReturnsTheRightValue()
+        {
+            List<int> numberList = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var result = numberList.Sum();
+            Assert.AreEqual(45, result);
+        }
+    }
+}
