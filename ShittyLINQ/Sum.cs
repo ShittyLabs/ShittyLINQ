40b680609d8b95c5a7473ea9d258777ed1a8f496
nestedl00p <nrosales@clubessential.com>
2018-10-31 23:57:01 -0500

    Sum added

---
+++ ShittyLINQ/Sum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShittyLINQ
+{
+    public static partial class Extensions
+    {
+        public static int Sum(this IEnumerable<int> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException("Source is null.");
+
+            int sum = 0;
+
+            foreach(var value in source)
+            {
+                sum += value;
+            }
+
+            return sum;
+        }
+    }
+}
