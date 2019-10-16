8bca890fd864a225e2126decdfa9674c1e761816
RichardVasquez <github.com@callahansplace.org>
2019-10-15 21:53:22 -0700

    Add ToHashSet and tests

---
+++ ShittyLINQ/ToHashSet.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShittyLINQ
+{
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// Creates a HashSet from an IEnumerable
+        /// </summary>
+        /// <typeparam name="TSource">Source type</typeparam>
+        /// <param name="source">Set of elements to turn into a hash set</param>
+        /// <returns>A HashSet from source</returns>
+        public static HashSet<TSource> ToHashSet<TSource>(this IEnumerable<TSource> source )
+        {
+            return ToHashSet(source, EqualityComparer<TSource>.Default);
+        }
+
+        /// <summary>
+        /// Creates a HashSet from an IEnumerable and an IEqualityComparer
+        /// </summary>
+        /// <typeparam name="TSource">Source Type</typeparam>
+        /// <param name="source">Set of elements to turn into a hash set</param>
+        /// <param name="comparer"></param>
+        /// <returns>A HashSet from source using comparer</returns>
+        public static HashSet<TSource> ToHashSet<TSource>(this IEnumerable<TSource> source, IEqualityComparer<TSource> comparer)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var hs = new HashSet<TSource>(comparer ?? EqualityComparer<TSource>.Default);
+
+            foreach (TSource item in source)
+            {
+                hs.Add(item);
+            }
+
+            return hs;
+        }
+    }
+}
---
+++ ShittyLinqTests/ToHashSetTests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using ShittyLINQ;
+using ShittyTests.TestHelpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShittyTests
+{
+    [TestClass]
+    public class ToHashSetTests
+    {
+        [TestMethod]
+        public void ToHashSet_Numbers()
+        {
+            IEnumerable<int> numbers = new[] {1, 2, 2, 3, 3, 3, 4, 4, 4};
+
+            var result = numbers.ToHashSet();
+            Assert.AreEqual(result.Count, 4);
+
+            foreach (var number in numbers)
+            {
+                Assert.IsTrue(result.Contains(number));
+            }
+        }
+
+        [TestMethod]
+        public void ToHashSet_Person1()
+        {
+            IEnumerable<Person> people =
+                new[]
+                {
+                    new Person("Adam", 20, "Arquitech", "Amber"),
+                    new Person("Brian", 45, "Arquitech", "Blue"),
+                    new Person("Brian", 45, "Arquitech", "Blue"),
+                    new Person("Charles", 32, "Arquitech", "Cyan"),
+                    new Person("Charles", 32, "Arquitech", "Cyan"),
+                    new Person("Charles", 32, "Arquitech", "Cyan"),
+                    new Person("Dani", 33, "Developer", "Deep Purple"),
+                    new Person("Dani", 33, "Developer", "Deep Purple"),
+                    new Person("Dani", 33, "Developer", "Deep Purple"),
+                    new Person("Dani", 33, "Developer", "Deep Purple")
+                };
+
+            var result = people.ToHashSet();
+            Assert.AreEqual(result.Count, 4);
+
+            foreach (var person in people)
+            {
+                Assert.IsTrue(result.Contains(person));
+            }
+        }
+
+        [TestMethod]
+        public void ToHashSet_Person2()
+        {
+            var rng = new Random();
+            var ages = new[]
+                       {
+                           rng.Next(20, 50), rng.Next(20, 50), rng.Next(20, 50), rng.Next(20, 50), rng.Next(20, 50),
+                           rng.Next(20, 50), rng.Next(20, 50), rng.Next(20, 50), rng.Next(20, 50), rng.Next(20, 50)
+                       };
+
+            IEnumerable<Person> people =
+                new[]
+                {
+                    new Person("Adam", ages[0], "Arquitech", "Amber"),
+                    new Person("Brian", ages[1], "Arquitech", "Blue"),
+                    new Person("Brian", ages[2], "Arquitech", "Blue"),
+                    new Person("Charles", ages[3], "Arquitech", "Cyan"),
+                    new Person("Charles", ages[4], "Arquitech", "Cyan"),
+                    new Person("Charles", ages[5], "Arquitech", "Cyan"),
+                    new Person("Dani", ages[6], "Developer", "Deep Purple"),
+                    new Person("Dani", ages[7], "Developer", "Deep Purple"),
+                    new Person("Dani", ages[8], "Developer", "Deep Purple"),
+                    new Person("Dani", ages[9], "Developer", "Deep Purple")
+                };
+
+            var result = people.ToHashSet(new WeakCompare());
+            Assert.AreEqual(result.Count, 4);
+
+            foreach (var person in people)
+            {
+                Assert.IsTrue(result.Contains(person));
+            }
+        }
+    }
+
+    /// <inheritdoc />
+    /// <summary>
+    /// This is weak comparator that ignores the age of a Person
+    /// </summary>
+    internal class WeakCompare : IEqualityComparer<Person>
+    {
+        public bool Equals(Person x, Person y)
+        {
+            if (x == null | y == null)
+            {
+                return false;
+            }
+
+            return x.Name.Equals(y.Name) &&
+                   x.Job.Equals(y.Job) &&
+                   x.FavoriteColor.Equals(y.FavoriteColor);
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Weak hash code that ignores the age of the person
+        /// </summary>
+        /// <param name="obj">Person to get the hash code of</param>
+        /// <returns>hash code of the person</returns>
+        public int GetHashCode(Person obj)
+        {
+            return TestHelper.CombineHashCodes(
+                obj.Name,
+                TestHelper.CombineHashCodes(
+                    obj.Job, obj.FavoriteColor
+                )
+            );
+        }
+    }
+}
